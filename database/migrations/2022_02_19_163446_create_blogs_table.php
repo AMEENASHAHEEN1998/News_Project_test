6074baadc479c447db1485c6dc870e8b8130ecb1
AMEENASHAHEEN1998 <ameena.sh.98@gmail.com>
2022-02-19 19:26:09 +0200

    database and migration

---
+++ database/migrations/2022_02_19_163446_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('short_description');
+            $table->text('long_description');
+            $table->string('image');
+            $table->foreignId('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
